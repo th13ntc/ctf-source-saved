b8de45aa73222d6cab62befde4d0018fb284c361
Thien Tran-Chi <47786126+th13ntc@users.noreply.github.com>
2022-06-29 22:40:50 +0700

    Delete picoCTF2022/bundle/client directory

--- picoCTF2022/bundle/client/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { render } from "react-dom";
-import { App } from "./components/app";
-
-import "./index.scss";
-
-render(<App />, document.getElementById("root"));
